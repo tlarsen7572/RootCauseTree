14deaebdfcb98479e4584f2be604003afb026613
TOMLAPTOP\Tom <larsenthomasj@gmail.com>
2017-02-01 06:05:40 -0600

    Minor sqlitedb test adjustments to make the tests independent.  Added functionality to add nodes to database.

--- RootCauseTreeCore/SqliteDb.cs
+++ RootCauseTreeCore/SqliteDb.cs
@@ -5,7 +5,7 @@ using System.Data.SQLite;
 
 namespace com.PorcupineSupernova.RootCauseTreeCore
 {
-    class SqliteDb
+    class SqliteDb : IRootCauseDb
     {
         private static SqliteDb _Db = new SqliteDb();
 
@@ -20,26 +20,119 @@ namespace com.PorcupineSupernova.RootCauseTreeCore
 
         public bool CreateNewFile(string path)
         {
-            CreateDbFile(path);
             CurrentFile = path;
+            CreateDbFile();
             return true;
         }
 
-        private void CreateDbFile(string path)
+        public bool InsertTopLevel(Node node)
         {
-            SQLiteConnection.CreateFile(path);
-            string connStr = string.Concat("Data Source=", path, ";Version=3;");
-            var conn = new SQLiteConnection(connStr);
-            string sql =    @"DROP TABLE IF EXISTS toplevel;
+            var conn = CreateConnection();
+            string sql =    @"INSERT INTO toplevel (nodeid) VALUES ($newid);
+                            INSERT INTO nodes (nodeid,nodetext) VALUES ($newid,$newtext);";
+            var command = new SQLiteCommand(sql, conn);
+            command.Parameters.AddWithValue("$newid", node.NodeId);
+            command.Parameters.AddWithValue("$newtext", node.Text);
+            conn.Open();
+            int records = command.ExecuteNonQuery();
+            CleanUp(conn, command);
+
+            return records == 1 ? true : false;
+        }
+
+        public bool AddLink(Node startNode, Node endNode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveLink(Node startNode, Node endNode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ChangeNodeText(Node node, string newText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddNode(Node startNode, Node newNode)
+        {
+            var conn = CreateConnection();
+            string sql =    @"INSERT INTO nodes (nodeid,nodetext) VALUES ($newid,$newtext);
+                            INSERT INTO hierarchy (parentid,childid) VALUES ($parentid,$newid);";
+            var command = new SQLiteCommand(sql, conn);
+            command.Parameters.AddWithValue("$newid", newNode.NodeId);
+            command.Parameters.AddWithValue("$newtext", newNode.Text);
+            command.Parameters.AddWithValue("$parentid", startNode.NodeId);
+            conn.Open();
+            int records = command.ExecuteNonQuery();
+            CleanUp(conn, command);
+
+            return records == 1 ? true : false;
+        }
+
+        public bool RemoveNode(Node removeNode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveNodeChain(Node removeNode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNode(Node node, Node targetNode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UndoMoveNode(Node node, Node targetNode, IEnumerable<Node> oldParents)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UndoRemoveNodeChain(Node removeNode, IEnumerable<Node> oldParents)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UndoRemoveNode(Node removeNode, IEnumerable<Node> oldParents, IEnumerable<Node> oldNodes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveTopLevel(Node node)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreateDbFile()
+        {
+            SQLiteConnection.CreateFile(CurrentFile);
+            var conn = CreateConnection();
+            string sql = @"DROP TABLE IF EXISTS toplevel;
                             DROP TABLE IF EXISTS nodes;
                             DROP TABLE IF EXISTS hierarchy;
-                            CREATE TABLE toplevel (nodeid int);
-                            CREATE TABLE nodes (nodeid int, nodetext text);
-                            CREATE TABLE hierarchy (parentid int, childid int);";
+                            CREATE TABLE toplevel (nodeid BIGINT, PRIMARY KEY (nodeid)) WITHOUT ROWID;
+                            CREATE TABLE nodes (nodeid BIGINT, nodetext text, PRIMARY KEY (nodeid)) WITHOUT ROWID;
+                            CREATE TABLE hierarchy (parentid BIGINT, childid BIGINT, PRIMARY KEY (parentid, childid)) WITHOUT ROWID;";
             var command = new SQLiteCommand(sql, conn);
             conn.Open();
             command.ExecuteNonQuery();
+            CleanUp(conn, command);
+        }
+
+        private SQLiteConnection CreateConnection()
+        {
+            string connStr = string.Concat("Data Source=", CurrentFile, ";Version=3;");
+            return new SQLiteConnection(connStr);
+        }
+
+        private void CleanUp(SQLiteConnection conn, SQLiteCommand command)
+        {
+            command.Dispose();
             conn.Close();
+            conn.Dispose();
         }
     }
 }
--- RootCauseTreeTests/SqliteDbTests.cs
+++ RootCauseTreeTests/SqliteDbTests.cs
@@ -17,18 +17,21 @@ namespace com.PorcupineSupernova.RootCauseTreeTests
         }
 
         [TestMethod]
-        public void CreateNewFile()
+        public void TestSqliteDbInterface()
         {
-            SqliteDb db = SqliteDb.GetInstance();
-            string file = string.Concat(System.IO.Directory.GetCurrentDirectory(), "test.rootcause");
-            bool result = db.CreateNewFile(file);
-            Assert.AreEqual(true, result);
-            Assert.IsTrue(System.IO.File.Exists(file));
-            string connStr = string.Concat("Data Source=", file, ";Version=3;");
-            var conn = new SQLiteConnection(connStr);
+            //Run tests
+            TestCreateNewFile();
+            TestCreateProblemStatement();
+            TestCreateNewNode();
+        }
+
+        //TESTS
+        private void TestCreateNewFile()
+        {
+            SQLiteConnection conn = CreateNewDbAndOpen("Tester.rootcause");
+
             string sql = "SELECT name FROM sqlite_master WHERE type='table' AND name IN ('toplevel','nodes','hierarchy') ORDER BY name;";
             var command = new SQLiteCommand(sql, conn);
-            conn.Open();
             SQLiteDataReader reader = command.ExecuteReader();
             int records = 0;
             string[] names = new string[3];
@@ -37,9 +40,92 @@ namespace com.PorcupineSupernova.RootCauseTreeTests
                 names[records] = reader["name"] as string;
                 records++;
             }
-            conn.Close();
             Assert.AreEqual(3, records);
             Assert.AreEqual("hierarchy,nodes,toplevel", string.Join(",", names));
+
+            CleanUpDbObjects(conn, command, reader);
+        }
+
+        private void TestCreateProblemStatement()
+        {
+            SQLiteConnection conn = CreateNewDbAndOpen("Tester.rootcause");
+
+            var node = NodeFactory.CreateProblem("This is my problem", SequentialId.NewId());
+            SqliteDb.GetInstance().InsertTopLevel(node);
+            string nodeId = node.NodeId.ToString();
+            string sql = $"SELECT count(*) AS result FROM toplevel WHERE nodeid = '{nodeId}'";
+            var command = new SQLiteCommand(sql, conn);
+            SQLiteDataReader reader = command.ExecuteReader();
+            reader.Read();
+            Assert.AreEqual("1", reader["result"].ToString());
+            reader.Close();
+            command.Dispose();
+
+            sql = $"SELECT * FROM nodes WHERE nodeid = '{nodeId}'";
+            command = new SQLiteCommand(sql, conn);
+            reader = command.ExecuteReader();
+            reader.Read();
+            Assert.AreEqual(nodeId, reader["nodeid"].ToString());
+            Assert.AreEqual("This is my problem", reader["nodetext"]);
+
+            CleanUpDbObjects(conn,command, reader);
+        }
+
+        private void TestCreateNewNode()
+        {
+            SQLiteConnection conn = CreateNewDbAndOpen("Tester.rootcause");
+
+            var problem = NodeFactory.CreateProblem("Problem", SequentialId.NewId());
+            SqliteDb.GetInstance().InsertTopLevel(problem);
+
+            var node = NodeFactory.CreateCause("Node 1", SequentialId.NewId());
+            SqliteDb.GetInstance().AddNode(problem,node);
+
+            string sql = $"SELECT * FROM nodes WHERE nodeid = '{node.NodeId}'";
+            var command = new SQLiteCommand(sql, conn);
+            SQLiteDataReader reader = command.ExecuteReader();
+            reader.Read();
+            Assert.AreEqual("Node 1", reader["nodetext"]);
+            reader.Close();
+            command.Dispose();
+
+            sql = $"SELECT count(*) AS result FROM hierarchy WHERE parentid = '{problem.NodeId}' AND childid = '{node.NodeId}';";
+            command = new SQLiteCommand(sql, conn);
+            reader = command.ExecuteReader();
+            reader.Read();
+            Assert.AreEqual("1", reader["result"].ToString());
+
+            CleanUpDbObjects(conn, command, reader);
+        }
+
+        //UTILITIES
+        private SQLiteConnection CreateNewDbAndOpen(string fileName)
+        {
+            SqliteDb db = SqliteDb.GetInstance();
+            string filePath = GetPath(fileName);
+            bool result = db.CreateNewFile(filePath);
+            string connStr = string.Concat("Data Source=", filePath, ";Version=3;");
+            SQLiteConnection conn = new SQLiteConnection(connStr);
+            conn.Open();
+            return conn;
+        }
+
+        private SQLiteConnection CreateConnection(string fileName)
+        {
+            return new SQLiteConnection(string.Concat("Data Source=", GetPath(fileName), ";Version=3;"));
+        }
+
+        private string GetPath(string fileName)
+        {
+            return string.Concat(System.IO.Directory.GetCurrentDirectory(), @"\", fileName);
+        }
+
+        private void CleanUpDbObjects(SQLiteConnection conn, SQLiteCommand command,SQLiteDataReader reader)
+        {
+            reader.Close();
+            command.Dispose();
+            conn.Close();
+            conn.Dispose();
         }
     }
 }
